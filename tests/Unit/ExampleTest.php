63571a1622073b3230c4f79042e83dc052e1426d
nakachi <nakachi@ad-scope.co.jp>
2024-06-01 18:46:56 +0900

    sqliteを有効にした

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,10 +2,22 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // テスト用データベースに seed を実行する
+        Artisan::call('migrate:fresh --seed');
+    }
     /**
      * A basic test example.
      *
