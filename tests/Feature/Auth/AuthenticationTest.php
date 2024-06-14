9bc49c6d4b5b7a5cba86fd352a17e379657aba84
nakachi <nakachi@ad-scope.co.jp>
2024-06-14 17:49:09 +0900

    CIでテストが失敗するのでファイルを修正した

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -4,34 +4,43 @@ namespace Tests\Feature\Auth;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
 
-    // public function test_users_can_authenticate_using_the_login_screen(): void
-    // {
-    //     $user = User::factory()->create();
+    public function setUp(): void
+    {
+        parent::setUp();
 
-    //     $response = $this->post('/login', [
-    //         'email' => $user->email,
-    //         'password' => 'password',
-    //     ]);
+        // テスト用データベースに seed を実行する
+        Artisan::call('migrate:fresh --seed');
+    }
 
-    //     $this->assertAuthenticated();
-    //     $response->assertNoContent();
-    // }
+    public function test_users_can_authenticate_using_the_login_screen(): void
+    {
+        $user = User::factory()->create();
 
-    // public function test_users_can_not_authenticate_with_invalid_password(): void
-    // {
-    //     $user = User::factory()->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
 
-    //     $this->post('/login', [
-    //         'email' => $user->email,
-    //         'password' => 'wrong-password',
-    //     ]);
+        $this->assertAuthenticated();
+        $response->assertNoContent();
+    }
 
-    //     $this->assertGuest();
-    // }
+    public function test_users_can_not_authenticate_with_invalid_password(): void
+    {
+        $user = User::factory()->create();
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'wrong-password',
+        ]);
+
+        $this->assertGuest();
+    }
 }
