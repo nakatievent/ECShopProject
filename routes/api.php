1a172dfcba23c1a02cba771c92a07e2ac5362c1d
nakachi <nakachi@ad-scope.co.jp>
2024-06-17 14:41:22 +0900

    お気に入り機能、よくある質問、商品に関するコントローラーを作成

--- routes/api.php
+++ routes/api.php
@@ -14,25 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 // 商品
 Route::prefix('products')->name('products.')->group(function() {
-    // Route::get('list', App\Http\Controllers\Products\IndexController::class)->name('list');
-    // Route::post('search', App\Http\Controllers\Products\SearchController::class)->name('search');
-    // Route::post('detail', App\Http\Controllers\Products\DetailController::class)->name('detail');
+    Route::get('list', App\Http\Controllers\Products\IndexController::class)->name('list');
+    Route::post('search', App\Http\Controllers\Products\SearchController::class)->name('search');
+    Route::post('detail', App\Http\Controllers\Products\DetailController::class)->name('detail');
 });
 
 // お気に入り
 Route::prefix('favorite')->name('favorite.')->group(function() {
-    // Route::post('/favorite-add', App\Http\Controllers\Favorites\AddController::class)->name('favorite-add');
-    // Route::post('/favorite-delete', App\Http\Controllers\Favorites\DeleteController::class)->name('favorite-delete');
-    // Route::get('/favorite-list', App\Http\Controllers\Favorites\ListController::class)->name('favorite-list');
+    Route::post('/favorite-add', App\Http\Controllers\Favorites\AddController::class)->name('favorite-add');
+    Route::post('/favorite-delete', App\Http\Controllers\Favorites\DeleteController::class)->name('favorite-delete');
+    Route::get('/favorite-list', App\Http\Controllers\Favorites\ListController::class)->name('favorite-list');
 });
 
 // よくある質問
 Route::prefix('faq')->name('faq.')->group(function() {
-    // Route::get('/list/{shop_id}', App\Http\Controllers\Faq\ListController::class)->name('faq-list');
+    Route::get('/list/{shop_id}', App\Http\Controllers\Faqs\ListController::class)->name('faq-list');
 });
