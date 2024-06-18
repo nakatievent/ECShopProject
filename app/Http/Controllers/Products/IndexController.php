3d5ef854f2cace8203de0a7f6bf667ef50c41aeb
nakachi <nakachi@ad-scope.co.jp>
2024-06-18 10:47:42 +0900

    商品に関するコントローラーを追加した

--- app/Http/Controllers/Products/IndexController.php
+++ app/Http/Controllers/Products/IndexController.php
@@ -3,18 +3,44 @@
 namespace App\Http\Controllers\Products;
 
 use App\Http\Controllers\Controller;
+use App\Service\ProductService;
+use App\Service\ResponseService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class IndexController extends Controller
 {
+    private $ResponseService;
+    private $ProductService;
+
+
     /**
-     * Handle the incoming request.
+     * __construct
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  mixed $ProductService
+     * @param  mixed $ResponseService
+     * @return void
+     */
+    public function __construct(ProductService $ProductService, ResponseService $ResponseService)
+    {
+        $this->ResponseService = $ResponseService;
+        $this->ProductService  = $ProductService;
+    }
+
+
+    /**
+     * __invoke
+     *
+     * @param  mixed $request
+     * @return void
      */
     public function __invoke(Request $request)
     {
-        //
+        return $this->ResponseService->success(
+            [
+                'data' => $this->ProductService->getAllProducts(),
+            ],
+            $request->header('Accept')
+        );
     }
 }
