f2ddf4ee9793a8d2ed5d4ab6cfac34311f723c47
nakachi <nakachi@ad-scope.co.jp>
2024-06-26 17:44:34 +0900

    商品に関するテストデータ作成

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,28 +20,26 @@ class ProductSeeder extends Seeder
 
         for ($index = 1; $index <= 100; $index++) {
             $products[] = [
-                'name'          => '商品' . $index,
-                'brand_id'      => rand(1, 10),
-                'review_id'     => rand(1, 10),
-                'description'   => '商品' . $index . 'の説明',
-                'detail'        => '商品' . $index . 'の詳細',
-                'price'         => round(rand(100, 10000), -2),
-                'stock'         => rand(0, 100),
-                'sku'           => 'SKU' . $index,
-                'image_url'     => 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-                'image_alt'     => '商品' . $index . 'のalt属性の説明',
-                'size'          => null,
-                'status'        => true,
-                'discount'      => rand(0, 50) / 10,
-                'reviews_count' => rand(0, 100),
-                'created_at'    => now(),
-                'updated_at'    => now(),
+                'name'        => '商品' . $index,
+                'brand_id'    => rand(1, 10),
+                'description' => '商品' . $index . 'の説明',
+                'detail'      => '商品' . $index . 'の詳細',
+                'price'       => round(rand(100, 10000), -2),
+                'stock'       => rand(0, 100),
+                'sku'         => 'SKU' . $index,
+                'image_url'   => 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
+                'image_alt'   => '商品' . $index . 'のalt属性の説明',
+                'size'        => null,
+                'status'      => true,
+                'discount'    => rand(0, 50) / 10,
+                'created_at'  => now(),
+                'updated_at'  => now(),
             ];
 
             // バルクインサート
             if ($index % $bulkInsertCount === 0) {
                 DB::table('products')->insert($products);
-                $products = []; // インサート後に配列をリセット
+                $products = [];
             }
         }
 
